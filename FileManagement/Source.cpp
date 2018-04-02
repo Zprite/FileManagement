51c9ff670816561d391fdd63da178613324977b2
Zprite <Zprite@users.noreply.github.com>
2018-04-02 17:49:45 +0200

    Started working on file encryption, fixed a bug that caused fread to display one extra line at EOF

--- FileManagement/Source.cpp
+++ FileManagement/Source.cpp
@@ -1,30 +1,33 @@
 #define _CRT_SECURE_NO_DEPRECATE
+#define min_ascii 32
+#define max_ascii 254 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
 #include <conio.h>
 #include <Windows.h>
+#include <time.h>
 
 void fread(char*file_name);
 void fwrite(char*file_name);
+int fcopy(char*file_name, char* copy_name);
 int fdel(char*file_name, int delete_line);
 int freplace(char* file_name, int replace_line);
 int finsert(char* file_name, int replace_line);
+int fmerge(char*file_name, char* file2_name, char* mergefile_name);
+int fencrypt(char*file_name, char *encryptfile_name);
+
 int main()
 {
 	char file_name[20];
-	int replace_line;
-	printf("Input file name (.txt extension needed): ");
+	char encrypt_name[20];
+	printf("Input source file: ");
 	scanf("%s", file_name);
-	printf("\n\n");
-	fwrite(file_name);
+	printf("Input encrypted file name: ");
+	scanf("%s", encrypt_name);
+	fencrypt(file_name, encrypt_name);
 	fread(file_name);
-	printf("\n\n");
-	do{
-		printf("Insert to line: ");
-		scanf("%d", &replace_line);
-	} while (finsert(file_name, replace_line-1) == -1); // -1 because first line is line 1 instead of line 0 
-	fread(file_name);
+	fread(encrypt_name);
 	printf("\n\nPress any key to exit. . .");
 	_getch();
 }
@@ -38,22 +41,29 @@ void fread(char*file_name)
 	printf("\n\nShowing contents of '%s':\n", file_name);
 	fp = fopen(file_name, "r");
 	char readinput = fgetc(fp);
+	printf("  1  "); // UI start
 	while (readinput != EOF)
 	{
-		if (readinput == '\n'){
+		printf("%c", readinput);
+		if (readinput == '\n') 
+		{
 			nLines++;
 			nWords++;
+			// **UI**
+			if (nLines+1 > 99){printf( "%d  ", nLines + 1); } 
+			else if (nLines + 1 > 9 && nLines + 1 < 100) { printf(" %d  ", nLines + 1); }
+			else if (nLines < 9) { printf("  %d  ", nLines + 1); }
+			// **UI**
 		}
 		else if (readinput == ' ') nWords++;
 		else nChars++;
-		printf("%c", readinput);
 		i++;
 		readinput = fgetc(fp);
 	}
 	fclose(fp);
-	printf("\nNumber of lines in file: %d\n", nLines);
-	printf("Number of characters in file: %d\n", nChars); // Displays all characters except \n and space
-	printf("Number of words in file: %d\n", nWords-1); // -1 to exclude newline at the start
+	printf("\nNumber of lines in file: %d\n", nLines+1);
+	printf("Number of characters in file: %d\n", nChars); // counts all characters except \n and space
+	printf("Number of words in file: %d\n", nWords);
 }
 
 void fwrite(char*file_name)
@@ -77,7 +87,7 @@ void fwrite(char*file_name)
 int fdel(char*file_name, int delete_line)
 {
 	FILE *fp, *fp2;
-	int nLine = 0;
+	int nLine = 1;
 	fp = fopen(file_name, "r");
 	fp2 = fopen("copy.txt", "w");
 	char readinput = fgetc(fp);
@@ -106,7 +116,7 @@ int fdel(char*file_name, int delete_line)
 int freplace(char* file_name, int replace_line)
 {
 	FILE *fp, *fp2;
-	int nLine = 0;
+	int nLine = 1;
 	int has_replaced = 0;
 	fp = fopen(file_name, "r");
 	fp2 = fopen("copy.txt", "w");
@@ -143,7 +153,8 @@ int freplace(char* file_name, int replace_line)
 int finsert(char* file_name, int replace_line)
 {
 	FILE *fp, *fp2;
-	int nLine = 0;
+	char input_buffer[100];
+	int nLine = 1;
 	fp = fopen(file_name, "r");
 	fp2 = fopen("copy.txt", "w");
 	char readinput = fgetc(fp);
@@ -154,7 +165,6 @@ int finsert(char* file_name, int replace_line)
 		else 
 		{
 			getchar(); // Getchar to flush the newline from scanf
-			char input_buffer[100];
 			printf("Input to line %d:", replace_line + 1);
 			fgets(input_buffer, sizeof input_buffer, stdin);
 			fputs(input_buffer, fp2);
@@ -164,15 +174,126 @@ int finsert(char* file_name, int replace_line)
 		if (readinput == '\n') { nLine++; }
 		readinput = fgetc(fp);
 	}
+	if (replace_line >= nLine)
+	{
+		if (nLine + 100 < replace_line) // Exit with error code if a large number of newlines is required. This is to prevent program from making dangerous trash files :=)
+		{
+			printf("Error! Maximum newlines to add is 100.\n");
+			remove("copy.txt");
+			return -1;
+		}
+		for (nLine; nLine < replace_line; nLine++)
+		{
+			fputc('\n', fp2);
+		}
+		printf("Input to line %d:", replace_line + 1);
+		getchar(); // Getchar to flush the newline from scanf
+		fgets(input_buffer, sizeof input_buffer, stdin);
+		fputs(input_buffer, fp2);
+	}
 	fclose(fp);
 	fclose(fp2);
-	if (replace_line > nLine)
-	{
-		printf("ERROR, LINE %d does not exist !!\n", replace_line);
-		remove("copy.txt");
-		return -1;
-	}
 	remove(file_name);
 	rename("copy.txt", file_name);
 	return 0;
 }
+
+int fcopy(char*file_name, char* copy_name)
+{
+	FILE *fp, *fp2;
+	fp = fopen(file_name, "r");
+	fp2 = fopen(copy_name, "w");
+	char readinput = getc(fp);
+	while (readinput != EOF)
+	{
+		fputc(readinput, fp2);
+		readinput = getc(fp);
+	}
+	fclose(fp);
+	fclose(fp2);
+	printf("Successfully copied contents of '%s' to '%s'.\n", file_name, copy_name);
+	return 0;
+}
+
+int fmerge(char*file_name, char* file2_name, char* mergefile_name)
+{
+	FILE *fp, *fp2, *fmp;
+	fp = fopen(file_name, "r");
+	fp2 = fopen(file2_name, "r");
+	fmp = fopen(mergefile_name, "w");
+	if (fp == NULL || fp2 == NULL || fmp == NULL)
+	{
+		printf("Error opening file..!!\n");
+		fclose(fp);
+		fclose(fp2);
+		fclose(fmp);
+		return-1;
+	}
+	char readinput = fgetc(fp);
+	while (readinput != EOF)
+	{
+		putc(readinput, fmp);
+		readinput = fgetc(fp);
+	}
+	fclose(fp);
+	putc('\n', fmp);
+	readinput = fgetc(fp2);
+	while (readinput != EOF)
+	{
+		putc(readinput, fmp);
+		readinput = fgetc(fp2);
+	}
+	fclose(fp2);
+	fclose(fmp);
+	printf("Files successfully merged.\n");
+	return 0;
+}
+
+int fencrypt(char*file_name, char *encryptfile_name)
+{
+	FILE *fp, *efp;
+	int decrypt_code[4];
+	fp = fopen(file_name, "r");
+	efp = fopen(encryptfile_name, "w");
+	if (fp == NULL || efp == NULL)
+	{
+		printf("Error opening file..!");
+		fclose(fp); 
+		fclose(efp);
+		remove(encryptfile_name);
+		return -1;
+	}
+	srand(time(NULL));
+	printf("IMPORTANT!! DECRYPTION CODE FOR '%s' is: ", encryptfile_name);
+	for (int i = 0; i < 4; i++) // GENERATE ENCRYPTION CODE
+	{
+		decrypt_code[i] = (rand() % 99);
+		if (decrypt_code[i] < 10)
+			printf("0%d", decrypt_code[i]);
+		else
+		printf("%d", decrypt_code[i]);
+	}
+	printf("\n");
+	int counter = 0;
+	char readinput = fgetc(fp);
+	while (readinput != EOF)
+	{
+		printf("Shifting by %d\n", decrypt_code[counter]);
+		fputc(readinput + decrypt_code[counter], efp);
+		readinput = fgetc(fp);
+		counter++;
+		counter = counter % 4;
+	}
+	printf("Successfully encrypted file!\n");
+	fclose(fp);
+	fclose(efp);
+	return 0;
+}
+
+int fdecrypt(char*decrypt_file, char* outfile)
+{
+	int decrypt_code[4];
+	int codein[8];
+	printf("Input decryption code (example: '11023398') : ");
+}
+
